debbdbc40adf3283702aff509f4c9628efc6b4d5
Abderrahmen <abderrahmenmalouche12@gmail.com>
2024-01-31 22:38:35 +0100

    added the message

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,19 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef,useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Specialheading from '../../special-heading/Specialheading';
+import Popup from 'reactjs-popup';
 import "./Contact.css"
 const Contact = () => {
     const form = useRef();
   
+    const [isSubmitting, setIsSubmitting] = useState(false);
+     const [message, setMessage] = useState('');
     const sendEmail = (e) => {
       e.preventDefault();
+      setIsSubmitting(true);
+         setMessage('Sending...');
   
       emailjs.sendForm('service_bopmfzi', 'template_h4vso8a', form.current, 'hXyjrdnb9mNzwiWZS')
         .then((result) => {
             console.log(result.text);
-            console.log("message sent")
+            setMessage('Message sent successfully!');
+            setIsSubmitting(false);
         }, (error) => {
             console.log(error.text);
+            setMessage('Failed to send message, please try again.');
+            setIsSubmitting(false);
         });
     };
   
@@ -27,7 +35,12 @@ const Contact = () => {
                 <input id="email" type="email" name="user_email" placeholder='Email' />
                 </div>
                 <textarea id="message"name="message" placeholder='Message' />
-                <input id="submit" type="submit" value="Send" />
+                <Popup trigger=
+                {<input id="submit" type="submit" value="Send" disabled={isSubmitting} />}
+                position="right center">
+                <div className='popup'>Your message is Sent</div>
+                </Popup>
+               
             </form>
             </div>
         </div>
